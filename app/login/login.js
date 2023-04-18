40684b2f75833a84ff627ec4b6b690a31536d9e4
Mike Meier <m0.0m58@yahoo.de>
2023-04-18 21:23:51 +0200

    set

--- app/login/login.js
+++ app/login/login.js
@@ -2,21 +2,10 @@
  * init function when body loads in index.html
  */
 function loginInit() {
-    setTimeout(changeColorsOfLoginScreen, 1000);
+    changeColorsOfLoginScreen();
 }
 
-/**
- * changing the colors of the login screen and moving the logo to the left after 1 second
- */
-function changeColorsOfLoginScreen() {
-    document.getElementById('loginPage').classList.add('pageColorChange');
-    document.getElementById('loginLogoWhite').classList.add('logoMovement');
-    document.getElementById('loginLogoWhite').classList.add('no-opacity');
-    document.getElementById('loginLogoBlue').classList.add('logoMovement');
-    document.getElementById('loginLogoBlue').classList.remove('no-opacity');
-    document.getElementById('loginBox').classList.remove('no-opacity');
-    document.getElementById('loginSignupBox').classList.remove('no-opacity');
-}
+
 
 /**
  * todo
@@ -53,4 +42,17 @@ function backToLogin() {
 
 function sendResetEmail() {
     
+}/**
+ * changing the colors of the login screen and moving the logo to the left after 1 second
+ */
+function changeColorsOfLoginScreen() {
+    setTimeout(() => {
+        document.getElementById('loginPage').classList.add('pageColorChange');
+        document.getElementById('loginLogoWhite').classList.add('logoMovement');
+        document.getElementById('loginLogoWhite').classList.add('no-opacity');
+        document.getElementById('loginLogoBlue').classList.add('logoMovement');
+        document.getElementById('loginLogoBlue').classList.remove('no-opacity');
+        document.getElementById('loginBox').classList.remove('no-opacity');
+        document.getElementById('loginSignupBox').classList.remove('no-opacity');
+    }, 1000);
 }

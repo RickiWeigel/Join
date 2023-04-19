ffcc3e0d447cc6ec4bee1a322c1537877af576d5
Mike Meier <m0.0m58@yahoo.de>
2023-04-19 21:16:00 +0200

    login section design ready / mobile design soon

--- app/login/login.js
+++ app/login/login.js
@@ -5,15 +5,6 @@ function loginInit() {
     changeColorsOfLoginScreen();
 }
 
-
-
-/**
- * todo
- */
-function loadSignUpBox() {
-
-}
-
 /**
  * todo
  */
@@ -21,17 +12,11 @@ function loginUser() {
 
 }
 
+
 /**
  * todo
  */
 function registerNewUser() {
-
-}
-
-/**
- * todo
- */
-function backToLogin() {
 
 }
 
@@ -42,7 +27,51 @@ function backToLogin() {
 
 function sendResetEmail() {
     
-}/**
+}
+
+
+/**
+ * go back to login section 
+ */
+function backToLogin() {
+    document.getElementById('loginBox').classList.remove('d-none');
+    document.getElementById('signupBox').classList.add('d-none');
+    document.getElementById('forgotBox').classList.add('d-none');
+    document.getElementById('resetbox').classList.add('d-none');
+    document.getElementById('loginLogoWhite').classList.add('no-opacity');
+    document.getElementById('loginLogoBlue').classList.remove('no-opacity');
+    document.getElementById('loginPage').classList.add('pageColorChange');
+    document.getElementById('loginSignupBox').classList.remove('d-none');
+}
+
+
+/**
+ * load forget password section 
+ */
+function showForgetPasswortSection() {
+    document.getElementById('loginBox').classList.add('d-none');
+    document.getElementById('forgotBox').classList.remove('d-none');
+    document.getElementById('loginLogoWhite').classList.remove('no-opacity');
+    document.getElementById('loginLogoBlue').classList.add('no-opacity');
+    document.getElementById('loginPage').classList.remove('pageColorChange');
+    document.getElementById('loginSignupBox').classList.add('d-none');
+}
+
+
+/**
+ * show sign up section 
+ */
+function loadSignUpBox() {
+    document.getElementById('loginBox').classList.add('d-none');
+    document.getElementById('signupBox').classList.remove('d-none');
+    document.getElementById('loginLogoWhite').classList.remove('no-opacity');
+    document.getElementById('loginLogoBlue').classList.add('no-opacity');
+    document.getElementById('loginPage').classList.remove('pageColorChange');
+    document.getElementById('loginSignupBox').classList.add('d-none');
+}
+
+
+/**
  * changing the colors of the login screen and moving the logo to the left after 1 second
  */
 function changeColorsOfLoginScreen() {

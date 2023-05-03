20131c97ae468174548ccc21a9bd641e5785a90c
Mike Meier <m0.0m58@yahoo.de>
2023-05-03 23:28:29 +0200

    #

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -8,12 +8,27 @@ async function contactsInit() {
 
 
 function renderContacts() {
-    
+
 }
 
 
 function renderContactsCards() {
     for(let i = 0; i < users[activeUser].contacts.length; i++) {
-        contactContent.innerHTML += users[activeUser].contacts[i];
+        contactContent.innerHTML += templateContact(users[activeUser].contacts[i], i);
     }
+}
+
+
+function templateContact(contact, index) {
+    return `
+    <div id="contactID${index}" class="contactCard">
+        <div class="contactAvatar" style="background-color:${contact.color}">
+            <span class="contactInitials">${contact.initials}</span>
+        </div>
+        <div class="contactData">
+            <div class="dataName">${contact.name}</div>
+            <div class="dataMail">${contact.email}</div>
+        </div>
+    </div>
+        `
 }

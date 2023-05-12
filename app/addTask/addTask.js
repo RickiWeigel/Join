1566c0ef9593c8f4e2272f30b63f2d968a8d35f1
Ricki Weigel <ricki.weigel@gmx.de>
2023-05-12 23:49:31 +0200

    finished addNewCategory

--- app/addTask/addTask.js
+++ app/addTask/addTask.js
@@ -8,7 +8,36 @@ let subTask;
 let priority;
 let taskStatus;
 let selectedCategory;
-
+let selectedColor;
+let categoryColor;
+let newCategoryName;
+let newCategory={};
+const colorActions = {
+  lightblue: () => {
+    selectedColor = "#8AA4FF";
+    categoryColor = selectedColor;
+  },
+  red: () => {
+    selectedColor = "#FF0000";
+    categoryColor = selectedColor;
+  },
+  green: () => {
+    selectedColor = "#2AD300";
+    categoryColor = selectedColor;
+  },
+  orange: () => {
+    selectedColor = "#FF8A00";
+    categoryColor = selectedColor;
+  },
+  pink: () => {
+    selectedColor = "#E200BE";
+    categoryColor = selectedColor;
+  },
+  blue: () => {
+    selectedColor = "#0038FF";
+    categoryColor = selectedColor;
+  },
+};
 
 async function addTaskInit() {
   mainInit();
@@ -24,16 +53,15 @@ $(function () {
   });
 });
 
-
-function clearBtnOnHover(){
-  document.getElementById('clearBtn').src = "/assets/img/functionButtons/icon_cancel_blue.png";
+function clearBtnOnHover() {
+  document.getElementById("clearBtn").src =
+    "/assets/img/functionButtons/icon_cancel_blue.png";
 }
 
-
-function clearBtnLeaveHover(){
-  document.getElementById('clearBtn').src = "/assets/img/functionButtons/icon_cancel.png";
+function clearBtnLeaveHover() {
+  document.getElementById("clearBtn").src =
+    "/assets/img/functionButtons/icon_cancel.png";
 }
-
 
 function renderContactsToAssign() {
   if (!contactsSelektorOpen) {
@@ -59,7 +87,7 @@ function renderCategories() {
   `;
     for (let i = 0; i < users[activeUser].taskCategories.length; i++) {
       document.getElementById("selectTaskCategory").innerHTML += `
-    <div onclick="setSelectedCategory("${users[activeUser].taskCategories[i].categoryName}")" class="dropdown-content"><span>${users[activeUser].taskCategories[i].categoryName}</span></div>
+    <div onclick="setSelectedCategory('${users[activeUser].taskCategories[i].name}')" class="dropdown-content"><span>${users[activeUser].taskCategories[i].categoryName}</span></div>
     `;
     }
     categorySelektorOpen = true;
@@ -100,15 +128,23 @@ async function addTask() {
     toDueDate: date.value,
     taskID: users[activeUser].userTasks.length,
     assignedTo: selectedContactsToAssign,
+    category: {
+      name: newCategory.name,
+      color: newCategory.color,
+    },
   });
-  await setItem(`users`, JSON.stringify(users));
+  users[activeUser].taskCategories.push(
+    newCategory
+  );
   resetInput();
+  await setItem(`users`, JSON.stringify(users));
 }
 
 function resetInput() {
   taskInputTitle = document.getElementById("taskTitle").value = "";
   date = document.getElementById("datepicker").value = "";
   description = document.getElementById("description").value = "";
+  renderShowCategory();
 }
 
 function showInviteNewContact() {
@@ -121,46 +157,87 @@ function hideInviteNewContact() {
   document.getElementById("showInviteNewContact").classList.add("d-none");
   document.getElementById("selectContacts").classList.remove("d-none");
   document.getElementById("contactsToAssign").classList.add("d-none");
-  renderContactsToAssign()
+  renderContactsToAssign();
 }
-
 
 async function addNewInviteContact() {
   let contactEmail = document.getElementById("inviteNewContact").value;
   users[activeUser].contacts.push({
     name: contactEmail,
-    email: 'contactEmail',
-    phone: '',
-    initials: '',
+    email: "contactEmail",
+    phone: "",
+    initials: "",
     color: getRandomColor(),
   });
   await setItem(`users`, JSON.stringify(users));
-  document.getElementById("inviteNewContact").value = '';
-  hideInviteNewContact()
-  renderContactsToAssign()
+  document.getElementById("inviteNewContact").value = "";
+  hideInviteNewContact();
+  renderContactsToAssign();
 }
 
-
-async function setSelectedCategory(value){
+function setSelectedCategory(value) {
   selectedCategory = value;
 }
 
+function addNewCategoryName() {
+  newCategoryName = document.getElementById("addNewCategory").value;
+  // document.getElementById("currentCategory").innerHTML = `
+  //   <div>
+  //     <span>${newCategoryValue.name}</span>
+  //     <div id="circle" class="colorCircle"></div>
+  //   </div>
+  //   <img onclick="hideNewCategory()" src="/assets/img/functionButtons/cancelBlue.png">
+  //   <img style="padding-left: 8px; padding-right: 8px;" src="/assets/img/functionButtons/trennstrich.png">
+  //   <img onclick="" src="/assets/img/functionButtons/checkedIconSelector.png">
+  // `;
+  // document.getElementById('circle').style.backgroundColor = color;
+}
 
-async function addNewCategory (){
+function addCategoryColor(color) {
+  addNewCategoryName();
+  colorActions[color]();
+  document.getElementById('lightblue').classList.remove("colorCircleActive");
+  document.getElementById('red').classList.remove("colorCircleActive");
+  document.getElementById('green').classList.remove("colorCircleActive");
+  document.getElementById('orange').classList.remove("colorCircleActive");
+  document.getElementById('pink').classList.remove("colorCircleActive");
+  document.getElementById('blue').classList.remove("colorCircleActive");
+  document.getElementById(color).classList.add("colorCircleActive");
+}
 
+function addNewCategoryFunction(){
+  newCategory.name = newCategoryName;
+  newCategory.color = categoryColor;
+  hideNewCategory();
+  document.getElementById('showCategory').innerHTML=`
+    <div id="currentCategory">
+      <span>${newCategory.name}</span>
+      <div id="circle" class="colorCircle"></div>
+    </div>
+    <img src="../../assets/img/functionButtons/selectorArrow.png">
+  `;
+  document.getElementById('circle').style.backgroundColor = newCategory.color;
+}
+
+function renderShowCategory(){
+  document.getElementById('showCategory').innerHTML=`
+    <div id="currentCategory"><span>Select task category</span></div>
+    <img src="../../assets/img/functionButtons/selectorArrow.png"> 
+  `;
 }
 
 
 function showNewCategory() {
   document.getElementById("showNewCategory").classList.remove("d-none");
+  document.getElementById("colorCircle").classList.remove("d-none");
   document.getElementById("showCategory").classList.add("d-none");
   document.getElementById("selectTaskCategory").classList.add("d-none");
 }
 
-
 function hideNewCategory() {
   document.getElementById("showNewCategory").classList.add("d-none");
+  document.getElementById("colorCircle").classList.add("d-none");
   document.getElementById("showCategory").classList.remove("d-none");
   document.getElementById("selectTaskCategory").classList.remove("d-none");
-  renderCategories()
+  renderCategories();
 }

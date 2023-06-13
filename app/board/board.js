9e40ba0a08b77b37dd43a100b07c016ccc843123
Ricki W <ricki.weigel@gmx.de>
2023-06-13 12:34:15 +0200

    show tasks in the right field

--- app/board/board.js
+++ app/board/board.js
@@ -1,43 +1,84 @@
+let completedTask = [];
+let currentSubtasks = [];
+
 async function boardInit() {
-  mainInit();
+  await mainInit();
+  groupTasksByProgressStatus(users[activeUser]);
 }
 
-let toDo = [];
-let inProgress = [];
-let awaitFeedback = [];
-let done = [];
-
 function groupTasksByProgressStatus(user) {
-  toDo = [];
-  inProgress = [];
-  awaitFeedback = [];
-  done = [];
+  resetTaskContainers();
   let userTasks = user.userTasks;
   for (let i = 0; i < userTasks.length; i++) {
     let task = userTasks[i];
-    sortProgress(task);
+    sortProgress(task, i);
   }
-  return {
-    toDo,
-    inProgress,
-    awaitFeedback,
-    done,
-  };
 }
 
-function sortProgress(task) {
+function sortProgress(task, i) {
   switch (task.progressStatus) {
     case "toDo":
-      toDo.push(task);
+      renderTodoTasks("toDoTasks", i);
       break;
     case "inProgress":
-      inProgress.push(task);
+      renderTodoTasks("inProgressTasks", i);
       break;
     case "awaitFeedback":
-      awaitFeedback.push(task);
+      renderTodoTasks("awaitFeedbackTasks", i);
       break;
     case "done":
-      done.push(task);
+      renderTodoTasks("doneTasks", i);
       break;
+  }
+}
+
+function renderTodoTasks(taskStatus, i) {
+  let userTask = users[activeUser].userTasks[i];
+  let priorityImageUrl = getPriorityImageUrl(userTask.priority);
+  document.getElementById(taskStatus).innerHTML +=`
+  <div class="taskCard">
+    <div class="categoryHeadline" style="background: ${userTask.category.color};">
+        <span>${userTask.category.name}</span>
+    </div>
+    <div class="taskHeadline">
+        <span>${userTask.taskTitle}</span>
+    </div>
+    <div class="taskDescription">
+        <span>${userTask.taskDescription}</span>
+    </div>
+    <div class="progressStatus">
+        <div class="progress"></div>
+        <span>0/${userTask.subtasks.length} Done</span>
+    </div>
+    <div class="taskFooter">
+        <div class="taskContacts" id="taskContacts">
+            <div class="contact box0">
+                <span id="contactInitials1">SM</span>
+            </div>
+            <div class="contact box1">
+                <span id="contactInitials2">BM</span>
+            </div>
+        </div>
+        <div class="taskPriority"><img src="${priorityImageUrl}"></div>
+    </div>
+  </div>
+  `;
+}
+
+
+function resetTaskContainers() {
+  document.getElementById("toDoTasks").innerHTML = "";
+  document.getElementById("inProgressTasks").innerHTML = "";
+  document.getElementById("awaitFeedbackTasks").innerHTML = "";
+  document.getElementById("doneTasks").innerHTML = "";
+}
+
+function getPriorityImageUrl(priority) {
+  if (priority == "Low") {
+    return "../../assets/img/board/iconLow.png";
+  } else if (priority == "Medium") {
+    return "../../assets/img/board/iconMid.png";
+  } else if (priority == "Urgent") {
+    return "../../assets/img/board/iconHard.png";
   }
 }

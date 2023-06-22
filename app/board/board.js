d8560702c5770faa449bca7aca1dc78231f158d4
Ricki Weigel <ricki.weigel@gmx.de>
2023-06-22 13:17:57 +0200

    added taskPopup

--- app/board/board.js
+++ app/board/board.js
@@ -1,7 +1,6 @@
 let completedTask = [];
 let currentSubtasks = [];
 
-
 async function boardInit() {
   await mainInit();
   groupTasksByProgressStatus(users[activeUser]);
@@ -20,19 +19,19 @@ function sortProgress(task, id) {
   switch (task.progressStatus) {
     case "toDo":
       renderTodoTasks("toDoTasks", id);
-      tasksInTodo++
+      tasksInTodo++;
       break;
     case "inProgress":
       renderTodoTasks("inProgressTasks", id);
-      tasksInProgress++
+      tasksInProgress++;
       break;
     case "awaitFeedback":
       renderTodoTasks("awaitFeedbackTasks", id);
-      tasksInAwaitFeedback++
+      tasksInAwaitFeedback++;
       break;
     case "done":
       renderTodoTasks("doneTasks", id);
-      tasksInDone++
+      tasksInDone++;
       break;
   }
 }
@@ -40,7 +39,7 @@ function sortProgress(task, id) {
 function renderTodoTasks(taskStatus, id) {
   let userTask = users[activeUser].userTasks[id];
   let priorityImageUrl = getPriorityImageUrl(userTask.priority);
-  document.getElementById(taskStatus).innerHTML +=`
+  document.getElementById(taskStatus).innerHTML += `
   <div class="taskCard">
     <div class="categoryHeadline" style="background: ${userTask.category.color};">
         <span>${userTask.category.name}</span>
@@ -52,7 +51,9 @@ function renderTodoTasks(taskStatus, id) {
         <span>${userTask.taskDescription}</span>
     </div>
     <div class="progressStatus">
-        <div class="progress"></div>
+        <div class="progress">
+          <div class="progressCompleted" style="width: 80%;"></div>
+        </div>
         <span>0/${userTask.subtasks.length} Done</span>
     </div>
     <div class="taskFooter">
@@ -63,25 +64,23 @@ function renderTodoTasks(taskStatus, id) {
     </div>
   </div>
   `;
-  renderContactInitials(id)
+  renderContactInitials(id);
 }
 
-function renderContactInitials(id){
+function renderContactInitials(id) {
   document.getElementById(`taskContacts${id}`).innerHTML = ``;
   let userContact = users[activeUser].userTasks[id].assignedTo;
 
   for (let j = 0; j < userContact.length; j++) {
     let newColor = userContact[j].color;
 
-
     if (userContact.length < 3) {
-    
       document.getElementById(`taskContacts${id}`).innerHTML += `
       <div class="contact box${j}" style="background-color: ${newColor};">
         <span id="contactInitials">${userContact[j].initials}</span>
       </div>`;
     } else {
-        document.getElementById(`taskContacts${id}`).innerHTML = `
+      document.getElementById(`taskContacts${id}`).innerHTML = `
         <div class="contact box${0}" style="background-color: ${newColor};">
           <span id="contactInitials">${userContact[0].initials}</span>
         </div>
@@ -92,13 +91,12 @@ function renderContactInitials(id){
 
       document.getElementById(`taskContacts${id}`).innerHTML += `
       <div class="contact boxRest">
-        <span id="contactInitials">+${userContact.length-2}</span>
+        <span id="contactInitials">+${userContact.length - 2}</span>
       </div>             
       `;
     }
   }
 }
-
 
 function resetTaskContainers() {
   document.getElementById("toDoTasks").innerHTML = "";
@@ -116,3 +114,20 @@ function getPriorityImageUrl(priority) {
     return "../../assets/img/board/iconHard.png";
   }
 }
+
+function openPopupTask() {
+  const popupContainer = document.getElementById('popupContainer');
+  
+  popupContainer.classList.remove('hidePopup');
+  popupContainer.classList.add('popupActive');
+}
+
+function hidePopupTask(){
+  const popupContainer = document.getElementById('popupContainer');
+  popupContainer.classList.remove('popupActive');
+  popupContainer.classList.add('hidePopup');
+}
+
+// white-space: nowrap; /* Verhindert den Zeilenumbruch */
+// overflow: hidden; /* Versteckt den überschüssigen Text */
+// text-overflow: ellipsis;

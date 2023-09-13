b5800553ad78614f1e9b4769764d2db9ed35ad45
Ricki Weigel <ricki.weigel@gmx.de>
2023-09-14 01:07:37 +0200

    #

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -1,4 +1,6 @@
 let contactContent = document.getElementById("contactsContent");
+let hidePopupStatus;
+
 
 async function contactsInit() {
   await mainInit();
@@ -9,32 +11,70 @@ function test() {
   console.log("test");
 }
 
-function slideInPopup() {
-  hidePopupStatus = 1;
+function slideInPopupAddContact() {
+  hidePopupStatus = false;
   const popupContainer = document.getElementById("popupContainer");
-  const popupContent = document.getElementById(
-    "slide-container-add-contact"
-  );
+  const popupContent = document.getElementById("slide-container-add-contact");
   popupContent.classList.remove("add-contact-slide-out-right");
-  popupContainer.classList.remove("hidePopup");
   popupContainer.classList.add("containerPopupActive");
   popupContent.classList.add("add-contact-slide-in-right");
 }
 
-function slideOutPopup() {
+function slideOutPopupAddContact() {
   const popupContainer = document.getElementById("popupContainer");
   const popupContent = document.getElementById("slide-container-add-contact");
   popupContent.classList.remove("add-contact-slide-in-right");
   popupContent.classList.add("add-contact-slide-out-right");
   popupContainer.classList.remove("containerPopupActive");
-  popupContainer.classList.add("hidePopup");
-  hidePopupStatus = 0;
+  hidePopupStatus = true;
 }
 
-function hidePopup() {
-  if (hidePopupStatus == 0) {
-    slideOutPopup();
-  } else {
-    hidePopupAddTask();
+
+function slideInPopupEdit() {
+  hidePopupStatus = false;
+  const popupContainer = document.getElementById("popupContainer");
+  const popupContent = document.getElementById("slide-container-edit-contact");
+  popupContent.classList.remove("edit-slide-out");
+  popupContainer.classList.add("containerPopupActive");
+  popupContent.classList.add("edit-slide-in");
+  console.log(users);
+}
+
+function slideOutPopupEdit() {
+  const popupContainer = document.getElementById("popupContainer");
+  const popupContent = document.getElementById("slide-container-edit-contact");
+  popupContent.classList.remove("edit-slide-in");
+  popupContent.classList.add("edit-slide-out");
+  popupContainer.classList.remove("containerPopupActive");
+  hidePopupStatus = true;
+}
+
+
+
+function sortAndGroupContactsByInitial() {
+ let contacts = users[activeUser].contacts;
+  
+  // Ein leeres Objekt erstellen, um die Kontakte nach Anfangsbuchstaben zu gruppieren
+  const contactRegisters = {};
+
+  // Durch jeden Kontakt iterieren
+  for (const contact of contacts) {
+    const initial = contact.name.charAt(0).toUpperCase(); // Erster Buchstabe des Namens extrahieren und in Großbuchstaben umwandeln
+
+    // Wenn das Register für diesen Anfangsbuchstaben noch nicht existiert, erstellen Sie es
+    if (!contactRegisters[initial]) {
+      contactRegisters[initial] = [];
+    }
+
+    // Kontakt zum entsprechenden Register hinzufügen
+    contactRegisters[initial].push(contact);
   }
+
+  // Die Kontaktregister nach dem Anfangsbuchstaben sortieren
+  const sortedRegisters = {};
+  Object.keys(contactRegisters).sort().forEach(initial => {
+    sortedRegisters[initial] = contactRegisters[initial];
+  });
+
+  return sortedRegisters;
 }
